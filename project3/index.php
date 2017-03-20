80bb3defc924c827b21e3b8eb2fb2c58897619e3
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2017-03-20 00:28:51 -0700

    added in parts of index page

---
+++ project3/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- metadata information -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tsarbucks</title>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100">
+  <link href="https://fonts.googleapis.com/css?family=Galada" rel="stylesheet">
+</head>
+<body>
+
+<!-- rest of the code goes here... -->
+
+<style>
+body {
+    font-family: 'Galada', cursive;
+    color: #006341;
+
+}
+
+h1 {
+    font-size: 100px;
+    position: absolute;
+    width: 100px;
+    height: 50px;
+    top: 50%;
+    left: 50%;
+    margin-left: -190px; /* margin is -0.5 * dimension */
+    margin-top: -155px; 
+}
+
+p {
+    position: absolute;
+    height: 50px;
+    top: 50%;
+    left: 50%;
+    margin-left: -190px; /* margin is -0.5 * dimension */
+    top: 270px;
+    left: 58%;
+}
+
+</style>
+
+<h1>Tsarbucks</h1>
+<p>Coffee. What else do you want?</p>
+
+</body>
+  <!-- js declarations at the end -->
+  <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="bower_components/tether/dist/js/tether.min.js"></script>
+  <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</html>
