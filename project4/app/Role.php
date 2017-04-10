826d3d6731cf0f91bc55ef5a92b5de446d51401f
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2017-04-10 10:37:53 -0700

    now I added the final db and model changes

---
+++ project4/app/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    // specify primary key
+    protected $primaryKey = "role_id";
+
+    // specifying that my table is called "users"
+    protected $table = "roles";
+
+    // specify the foreign key, since we're using a custom one
+    public function users() {
+        return $this->belongsToMany('App\User', 'user_roles', 'user_id', 'role_id');
+    }
+
+    // specify the many to many relation
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role',
+    ];
+}
